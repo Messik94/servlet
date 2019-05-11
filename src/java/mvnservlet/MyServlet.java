acc1b223fd0318c66ea9b16d8c915229edd3bca0
Marcin Stankiewicz <marcin.stankiewicz@int.pl>
2019-05-11 13:51:54 +0200

    First commit

---
+++ src/java/mvnservlet/MyServlet.java
@@ -0,0 +1,30 @@
+package mvnservlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        PrintWriter out = resp.getWriter();
+        out.println("Hello World from mvn!");
+
+        resp.sendRedirect(req.getContextPath() + "/another");
+//        getServletContext().getRequestDispatcher("/another").forward(req, resp);
+//        getServletContext().getRequestDispatcher("/another").include(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        PrintWriter out = resp.getWriter();
+        out.println("Hello post");
+    }
+}
